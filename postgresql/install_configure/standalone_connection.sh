76c4a4ef5f9bb2b0fb48a4a8a205aff5f19959b5
Sampath Hewage <samdhew@users.noreply.github.com>
2024-01-16 15:30:10 +0530

    Create standalone_connection.sh

---
+++ postgresql/install_configure/standalone_connection.sh
@@ -0,0 +1,8 @@
+# Connect to PostgreSQL server with specified host, username, database, and port
+psql -h 10.1.x.x -U postgres1 -d postgres1 -p 5432
+ 
+# List all databases in the PostgreSQL server
+\l
+ 
+# List all database users and their roles
+\du
